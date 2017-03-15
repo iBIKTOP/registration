ce5effb57e47e097dc13a13218567cb466b0c9a3
iBIKTOP <biktopbb@gmail.com>
2017-03-15 23:57:48 +0200

    Registration_1

---
+++ www/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Registration</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<form id="reg">
+		<p align="CENTER">AUTHORIZATION FORM</p>
+		<div class="left">
+			<label for="login">Login:</label><br>
+			<label for="pass">Password:</label>
+		</div>
+		<div class="left">
+			<input type="text" name="login" id="login"><br>
+			<input type="text" name="pass" id="pass"><br>
+		</div>
+		<div class="left">
+			<a href="">Registration</a>
+		</div>
+		<div class="right">
+			<input type="submit" name="enter" value="Enter">
+		</div>
+		
+
+	</form>
+</body>
+</html>
